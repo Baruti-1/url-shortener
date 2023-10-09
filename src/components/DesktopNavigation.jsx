ea7d1e15ca84050c7b24c4489c27223c139ab47d
Baruti-1 <barutibaruani1@gmail.com>
2023-10-09 10:20:16 +0300

    Create navigation menu for desktop and larger screens

---
+++ src/components/DesktopNavigation.jsx
@@ -0,0 +1,55 @@
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/images/logo.svg';
+
+const DesktopNavigation = () => {
+  return (
+    <div>
+      {/* nav container */}
+      <nav className="relative container mx-auto p-6">
+        {/* flex container for all items */}
+        <div className="flex items-center justify-between">
+          {/* flex container for logo/menu */}
+          <div className="flex items-center space-x-20">
+            {/* logo */}
+            <img src={logo} alt="logo" />
+            {/* left menu */}
+            <div className="hidden space-x-8 font-bold lg:flex">
+              <NavLink
+                to="#"
+                className="text-grayishViolet hover:text-veryDarkViolet"
+              >
+                Features
+              </NavLink>
+              <NavLink
+                to="#"
+                className="text-grayishViolet hover:text-veryDarkViolet"
+              >
+                Pricing
+              </NavLink>
+              <NavLink
+                to="#"
+                className="text-grayishViolet hover:text-veryDarkViolet"
+              >
+                Resources
+              </NavLink>
+            </div>
+          </div>
+          {/* right buttons menu */}
+          <div className="hidden items-center font-bold text-grayishViolet space-x-4 lg:flex">
+            <div className="hover:text-veryDarkViolet">Login</div>
+            <NavLink
+              to="#"
+              className="px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70"
+            >
+              Sign Up
+            </NavLink>
+          </div>
+          {/* todo hamburger menu */}
+        </div>
+        {/* display hamburger menu */}
+      </nav>
+    </div>
+  );
+};
+
+export default DesktopNavigation;
