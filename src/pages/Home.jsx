7a239f04394c98537f33866fac6992ca4423259b
Baruti-1 <barutibaruani1@gmail.com>
2023-10-12 17:50:28 +0300

    Create Call To Action (CTA) component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from '../components/Hero';
 import Shorten from '../components/Shorten';
 import Statistics from '../components/Statistics';
+import Cta from '../components/Cta';
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Hero />
       <Shorten />
       <Statistics />
+      <Cta />
     </>
   );
 };
