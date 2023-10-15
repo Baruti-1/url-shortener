86014bdc455123b561c99364bcd9c501d812a461
Baruti-1 <barutibaruani1@gmail.com>
2023-10-15 10:13:07 +0300

    Shorten valid long links

--- src/components/Shorten.jsx
+++ src/components/Shorten.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+// google-chrome --disable-web-security --user-data-dir="[some directory here]"
+
+import { useState } from 'react';
+import Spinner from './Spinner';
 
 const Shorten = () => {
+  const [urls, setUrls] = useState([]);
+  const [spinner, setSpinner] = useState(false);
+
   // Validate a URL
   function validURL(str) {
     var pattern = new RegExp(
@@ -15,24 +21,52 @@ const Shorten = () => {
     return !!pattern.test(str);
   }
 
+  let url = '';
+
   const formSubmit = (e) => {
     e.preventDefault();
 
     const input = document.getElementById('link-input');
     const errMsg = document.getElementById('err-msg');
 
-    if (input.value.trim() === '') {
+    url = input.value;
+
+    if (url.trim() === '') {
       errMsg.innerText = 'Please enter URL';
       input.classList.add('border-red');
-    } else if (!validURL(input.value.trim())) {
+    } else if (!validURL(url.trim())) {
       errMsg.innerText = 'Please enter a valid URL';
       input.classList.add('border-red');
     } else {
       errMsg.innerText = '';
       input.classList.remove('border-red');
-      console.log(input.value);
+      setSpinner(true);
+
+      const KEY = import.meta.env.VITE_API;
+
+      fetch('https://shrtlnk.dev/api/v2/link', {
+        method: 'POST',
+        headers: {
+          'api-key': KEY,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          url,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setUrls([...urls, data]);
+          setSpinner(false);
+        });
       input.value = '';
     }
+  };
+
+  const copyUrl = () => {
+    setCopy(!copy);
+    navigator.clipboard.writeText(urls.shrtlnk);
   };
 
   return (
@@ -50,8 +84,12 @@ const Shorten = () => {
             className="flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none"
             placeholder="Shorten a link here"
           />
-          <button className="px-10 py-3 text-white bg-cyan rounded-lg hover:bg-cyanLight focus:outline-none md:py-2">
-            Shorten It!
+          <button
+            className={`px-10 py-3 text-white ${
+              spinner ? 'bg-cyanLight' : 'bg-cyan'
+            } rounded-lg hover:bg-cyanLight focus:outline-none md:py-2`}
+          >
+            {spinner ? <Spinner /> : 'Shorten it!'}
           </button>
           {/* error message */}
           <div
@@ -59,42 +97,38 @@ const Shorten = () => {
             className="absolute left-7 bottom-3 text-red text-sm italic"
           ></div>
         </form>
-        {/* link 1 */}
-        <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
-          <p className="font-bold text-center text-veryDarkViolet md:text-left">
-            https://frontendmentor.io
-          </p>
-          <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
-            <div className="font-bold text-cyan">https://rel.ink/k4IKyk</div>
-            <button className="p-2 px-8 text-white bg-cyan rounded-lg hover:opacity-70 focus-outline-none">
-              Copy
-            </button>
+
+        {urls.map((url) => (
+          <div
+            key={url.key}
+            className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row"
+          >
+            <p className="font-bold text-center text-veryDarkViolet md:text-left">
+              {url.url}
+            </p>
+            <div
+              key={url.key}
+              className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0"
+            >
+              <div id="short-url" className="font-bold text-cyan">
+                {url.shrtlnk}
+              </div>
+              <button
+                id={url.key}
+                className="p-2 px-8 text-white bg-cyan rounded-lg hover:opacity-70 focus-outline-none"
+                onClick={() => {
+                  const btn = document.getElementById(`${url.key}`);
+                  btn.classList.add('bg-darkViolet');
+                  btn.innerText = 'Copied';
+
+                  navigator.clipboard.writeText(url.shrtlnk);
+                }}
+              >
+                Copy
+              </button>
+            </div>
           </div>
-        </div>
-        {/* link 2 */}
-        <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
-          <p className="font-bold text-center text-veryDarkViolet md:text-left">
-            https://twitter.com/frontendmentor
-          </p>
-          <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
-            <div className="font-bold text-cyan">https://rel.ink/k4IKyk</div>
-            <button className="p-2 px-8 text-white bg-darkViolet rounded-lg hover:opacity-70 focus-outline-none">
-              Copy
-            </button>
-          </div>
-        </div>
-        {/* link 1 */}
-        <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
-          <p className="font-bold text-center text-veryDarkViolet md:text-left">
-            https://linkedin.com/frontendmentor
-          </p>
-          <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
-            <div className="font-bold text-cyan">https://rel.ink/k4IKyk</div>
-            <button className="p-2 px-8 text-white bg-cyan rounded-lg hover:opacity-70 focus-outline-none">
-              Copy
-            </button>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from '../assets/images/spinner.svg';
+
+const Spinner = () => {
+  return (
+    <div>
+      <img src={spinner} alt="" className="h-8" />
+    </div>
+  );
+};
+
+export default Spinner;
